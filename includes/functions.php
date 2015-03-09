2975072395868ff9dfbc6f644293d5a5fadbcf9d
user0 <user0@192.168.0.2>
2015-03-09 15:48:21 +0100

    Added localization support, changed directories structure

---
+++ includes/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Return translated string. If there is no translation key is returned.
+ * 
+ * @global	Array	$_LANGUAGE	Translation array containing keys and its translations.
+ * @param	String	$key		Translation array key holding required translation
+ * 
+ * @return	String
+ */
+function __($key) {
+	global $_LANGUAGE;
+	
+	if( isset($_LANGUAGE[$key]) ) {
+		return $_LANGUAGE[$key];
+	} else {
+		return $key;
+	}
+}
+
+function _s($key) {
+	global $_LANGUAGE;
+	
+	$args = func_get_args();
+	$args = array_slice($args, 1);
+
+	if( isset($_LANGUAGE[$key]) ) {
+		return vsprintf($_LANGUAGE[$key], $args);
+	} else {
+		return $key;
+	}
+}
